741dd3ad0f49d53075202d75e73e644f3f15664d
kdkdhoho <hkim4410@naver.com>
2023-06-05 16:19:10 +0900

    refactor(SlackBot): 코드 리팩터링
    - 상수 추출
    - 공통 필드 추출

--- src/main/kotlin/dailyabsent/service/SlackBot.kt
+++ src/main/kotlin/dailyabsent/service/SlackBot.kt
@@ -1,40 +1,46 @@
 package dailyabsent.service
 
 import com.slack.api.Slack
+import com.slack.api.methods.MethodsClient
 import com.slack.api.methods.request.chat.ChatUpdateRequest
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+
+private const val TOKEN = "xoxb-5368094080258-5391821420192-5a2oU06hAJ26Jjgb6f3vjg9P"
+private const val CHANNEL = "C05AU2T8VNW"
 
 @Component
 class SlackBot {
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+    private val client: MethodsClient = Slack.getInstance().methods()
 
     fun publishMessage(message: String): String {
-        val client = Slack.getInstance().methods()
         val result = runCatching {
             client.chatPostMessage {
-                it.token("xoxb-5368094080258-5391821420192-5a2oU06hAJ26Jjgb6f3vjg9P")
-                    .channel("C05AU2T8VNW")
+                it.token(TOKEN)
+                    .channel(CHANNEL)
                     .text(message)
             }
         }.onFailure { e ->
-            println("Slack Send Error: " + e.message)
+            logger.error("Slack Send Message Error " + e.message)
         }
 
         return result.getOrThrow().ts!!
     }
 
     fun updateMessage(message: String, ts: String) {
-        val client = Slack.getInstance().methods()
         runCatching {
             client.chatUpdate(
                 ChatUpdateRequest.builder()
                     .ts(ts)
-                    .token("xoxb-5368094080258-5391821420192-5a2oU06hAJ26Jjgb6f3vjg9P")
-                    .channel("C05AU2T8VNW")
+                    .token(TOKEN)
+                    .channel(CHANNEL)
                     .text(message)
                     .build()
             )
         }.onFailure { e ->
-            println("Slack Send Error: " + e.message)
+            logger.error("Slack Update Message Error " + e.message)
         }
     }
 }
